679cc70a948cf0c367ed3d67b622f2e1a8a76019
19royy <64555594+19royy@users.noreply.github.com>
2022-02-23 21:33:35 -0800

    “done”

--- App.js
+++ App.js
@@ -6,28 +6,20 @@ import { REDIRECT_URI, SCOPES, CLIENT_ID, ALBUM_ID } from "./utils/constants";
 import Colors from "./Themes/colors"
 import Images from "./Themes/images"
 import millisToMinutesAndSeconds from "./utils/millisToMinuteSeconds.js"
+import { Ionicons } from '@expo/vector-icons';
+import { WebView } from 'react-native-webview';
+import { NavigationContainer} from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomePage from './HomePage.js';
 
 // Endpoints for authorizing with Spotify
 const discovery = {
   authorizationEndpoint: "https://accounts.spotify.com/authorize",
   tokenEndpoint: "https://accounts.spotify.com/api/token"
 };
-const DATA = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'First Item',
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Second Item',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
-  },
-];
 
 const f = false;
+const Stack = createStackNavigator();
 
 export default function App() {
   const [token, setToken] = useState("");
@@ -68,41 +60,46 @@ export default function App() {
     }
   }, [token]);
 
-  const renderItem = (item,index) => (
-    <View style={styles.songContainer}>
+  function Details({ route }) {
+    const link = route.params.link;
+    return (
+        <WebView source={{ uri: link }} />
 
-          <Text style={{color:"white"}}>{index + 1}</Text>
-          <Image source={{uri: item.album.images[0].url}} style={styles.spotify_logoflex}/>
-          <View style={{width: 120, }}>
-            <Text numberOfLines={1} style={{color:"white"}}>{item.name}</Text>
-            <Text numberOfLines={1} style={{color:"white"}}>{item.artists[0].name}</Text>
-          </View>
-          <Text style={{width: 50,color:"white"}} numberOfLines={1}>{item.album.name}</Text>
-          <Text style={{color:"white"}}>{millisToMinutesAndSeconds(item.duration_ms)}</Text>
-    </View>
-    /*
-    <View>
-      <Text style={{color:"white"}}>{item.album.name} </Text>
-      <Text style={{color:"white"}}>{item.artists[0].name} </Text>
-    </View>
-    */
-  );
+    );
+  }
 
-  //console.log(tracks[2]);
+  function Preview({ route }) {
+    const songlink= route.params.songlink;
+    return (
+        <WebView source={{ uri: songlink }} />
+    );
+  }
+
 
   if (token) { //false on default
     return (
-      <SafeAreaView style={styles.container}>
-        <View style={{flexDirection: "row", justifyContent: "center", alignItems: "center",}}>
-          <Image source={Images.spotify} style={styles.spotify_logo}/>
-          <Text style={{color:"white", fontSize: 20}}>My Top Tracks</Text>
-        </View>
-        <FlatList
-          data={tracks}
-          renderItem={({item,index}) => renderItem(item,index)}
-          keyExtractor={(item) => item.id}
-        />
-      </SafeAreaView>
+      <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Back" options={{headerShown: false}}>
+          { props => <HomePage {...props} mysongs={tracks} />}
+        </Stack.Screen>
+        <Stack.Screen options={{
+          title: 'Song Details',
+          headerStyle: {
+            backgroundColor: 'black',
+          },
+          headerTintColor: 'white',
+        }} name="deets" component={Details} />
+
+        <Stack.Screen options={{
+          title: 'Song Preview',
+          headerStyle: {
+            backgroundColor: 'black',
+          },
+          headerTintColor: 'white',
+        }} name="pre" component={Preview} />
+      </Stack.Navigator>
+    </NavigationContainer>
     );
   } else {
     return (
@@ -112,23 +109,6 @@ export default function App() {
           <Text style={styles.logo}>CONNECT WITH SPOTIFY</Text>
         </Pressable>
       </SafeAreaView>
-
-/* for the container
-      <SafeAreaView style={styles.container}>
-        <View style={styles.songContainer}>
-
-          <Text>1</Text>
-          <Image source={Images.spotify} style={styles.spotify_logoflex}/>
-          <View style={{width: 120,}}>
-            <Text numberOfLines={1}>Albumlongtitleyayayayaya</Text>
-            <Text>Artist</Text>
-          </View>
-          <Text style={{width: 50,}} numberOfLines={1}>Album</Text>
-          <Text >{millisToMinutesAndSeconds(555555)}</Text>
-        </View>
-
-      </SafeAreaView>
-*/
     );
   }
 }
@@ -175,3 +155,40 @@ const styles = StyleSheet.create({
   }
 
 });
+/*
+  function listScreen({navigation}) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={{flexDirection: "row", justifyContent: "center", alignItems: "center",}}>
+          <Image source={Images.spotify} style={styles.spotify_logo}/>
+          <Text style={{color:"white", fontSize: 20}}>My Top Tracks</Text>
+        </View>
+        <FlatList
+          data={tracks}
+          renderItem={({item,index}) => renderItem(item,index)}
+          keyExtractor={(item) => item.id}
+        />
+      </SafeAreaView>
+    );
+  }
+
+
+
+  const renderItem = (item,index) => (
+    <Pressable>
+        <View style={styles.songContainer}>
+          <Pressable onPress={(e) => playButton(e, item.preview_url)}>
+            <Ionicons name="play-circle-outline" size={28} color="green"/>
+          </Pressable>
+          <Image source={{uri: item.album.images[0].url}} style={styles.spotify_logoflex}/>
+          <View style={{width: 120, }}>
+            <Text numberOfLines={1} style={{color:"white"}}>{item.name}</Text>
+            <Text numberOfLines={1} style={{color:"white"}}>{item.artists[0].name}</Text>
+          </View>
+          <Text style={{width: 50,color:"white"}} numberOfLines={1}>{item.album.name}</Text>
+          <Text style={{color:"white"}}>{millisToMinutesAndSeconds(item.duration_ms)}</Text>
+       </View>
+    </Pressable>
+
+  );
+  */
